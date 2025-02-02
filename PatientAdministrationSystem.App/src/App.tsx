3a46c0a874da19c3af6543d252f637a7def28f5a
rennyeb <rennyeb@gmail.com>
2025-02-02 16:58:13 +0000

    Completed porting of search results to its own component.

--- PatientAdministrationSystem.App/src/App.tsx
+++ PatientAdministrationSystem.App/src/App.tsx
@@ -287,54 +287,10 @@ function App() {
 
         </table>
 
-
-
         {/* only show search results if a search has been performed */}
         {searchExecuted && (
-
-
-          <div>
-
-            <PatientVisitSearchResults searchResults={searchResults} />
-
-
-            <h1>Search Results</h1>
-
-            {searchResults.length ? (
-              <table border={1}>
-                <thead>
-                  {/* NB a more advanced implementation could support ascending/descending sorting options in the table headers , and pagination */}
-                  <tr>
-                    <th>Date</th>
-                    <th>Patient</th>
-                    <th>Hospital</th>
-                    <th>Details</th>
-                  </tr>
-                </thead>
-                <tbody>
-
-                  {searchResults.map((searchResult) => (
-                    <tr key={searchResult.visitId}>
-                      {/* NB already sorted into date descending order for usability - user is likely to be more interested in more recent visits */}
-                      <td>{searchResult.visitDateString}</td>
-                      {/* Concatenate the full patient name - easier on the user */}
-                      <td>{searchResult.patientFirstName} {searchResult.patientLastName}</td>
-                      <td>{searchResult.hospitalName}</td>
-                      {/* NB a more advanced implementation would show full details of the visit, the patient's email address, etc. */}
-                      <td><a href="#" onClick={() => alert('Not yet implemented')}>details</a></td>
-                    </tr>
-                  ))}
-                </tbody>
-
-              </table>
-
-            ) : "No results found.  Please check your search criteria and try again."}
-
-          </div>
-
+          <PatientVisitSearchResults searchResults={searchResults} />
         )}
-
-
 
       </div>
     </div>
--- PatientAdministrationSystem.App/src/PatientVisitSearchResults.tsx
+++ PatientAdministrationSystem.App/src/PatientVisitSearchResults.tsx
@@ -1,25 +1,47 @@
-import React, { useState } from "react";
-
-
+import React from "react";
 
 //TODO tidy, document
 interface PatientVisitSearchResultsProps {
   searchResults: any[];//TODO better type
 }
 
-
-//TODO rename
 const PatientVisitSearchResults: React.FC<PatientVisitSearchResultsProps> = ({ searchResults }) => {
-
-  
-  //TODO is state not required?
-  const [theSearchResults, setSearchResults] = useState(searchResults);
 
   //No events to handle
 
   return (
     <div>
-      hi
+      <h1>Search Results</h1>
+
+      {searchResults.length ? (
+        <table border={1}>
+          <thead>
+            {/* NB a more advanced implementation could support ascending/descending sorting options in the table headers , and pagination */}
+            <tr>
+              <th>Date</th>
+              <th>Patient</th>
+              <th>Hospital</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {searchResults.map((searchResult) => (
+              <tr key={searchResult.visitId}>
+                {/* NB already sorted into date descending order for usability - user is likely to be more interested in more recent visits */}
+                <td>{searchResult.visitDateString}</td>
+                {/* Concatenate the full patient name - easier on the user */}
+                <td>{searchResult.patientFirstName} {searchResult.patientLastName}</td>
+                <td>{searchResult.hospitalName}</td>
+                {/* NB a more advanced implementation would show full details of the visit, the patient's email address, etc. */}
+                <td><a href="#" onClick={() => alert('Not yet implemented')}>details</a></td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+
+      ) : "No results found.  Please check your search criteria and try again."}
     </div>
   );
 };
