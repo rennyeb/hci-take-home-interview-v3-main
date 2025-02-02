f486bea6dbe54b84df948264c8300c31083abf42
rennyeb <rennyeb@gmail.com>
2025-02-02 20:29:51 +0000

    Form submit button receives enter press.

--- PatientAdministrationSystem.App/src/PatientVisitSearchCriteria.tsx
+++ PatientAdministrationSystem.App/src/PatientVisitSearchCriteria.tsx
@@ -41,65 +41,64 @@ const PatientVisitSearchCriteria: React.FC<PatientVisitSearchCriteriaProps> = ({
     <div>
       {/* //NB could set the surname as the default field for focus */}
 
-      {/* //TODO not working */}
-      {/* <form> */}
-      <table>
-        <tbody>
-          <tr>
-            {/* NB could include client-side validation if this value not populated, highlight the field in red, etc.
+      <form>
+        <table>
+          <tbody>
+            <tr>
+              {/* NB could include client-side validation if this value not populated, highlight the field in red, etc.
             
             For now, leaving to the server to validate and return an error message. */}
-            <td>Last Name prefix *:</td>
-            <td>
-              <input
-                id="lastNamePrefix"
-                type="text"
-                placeholder='e.g. Smith or Smi'
-                value={lastName}
-                onChange={handleLastNamePrefixChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>First Name prefix:</td>
-            <td>
-              <input
-                id="firstNamePrefix"
-                type="text"
-                placeholder='e.g. John or Jo'
-                value={firstName}
-                onChange={handleFirstNamePrefixChange}
-              />
-            </td>
-          </tr>
+              <td>Last Name prefix *:</td>
+              <td>
+                <input
+                  id="lastNamePrefix"
+                  type="text"
+                  placeholder='e.g. Smith or Smi'
+                  value={lastName}
+                  onChange={handleLastNamePrefixChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>First Name prefix:</td>
+              <td>
+                <input
+                  id="firstNamePrefix"
+                  type="text"
+                  placeholder='e.g. John or Jo'
+                  value={firstName}
+                  onChange={handleFirstNamePrefixChange}
+                />
+              </td>
+            </tr>
 
-          <tr>
-            <td>Hospital:</td>
-            <td>
+            <tr>
+              <td>Hospital:</td>
+              <td>
 
 
-              <select id="hospital" value={JSON.stringify(selectedHospital)} onChange={handleHistorySelectedOptionChange}>
+                <select id="hospital" value={JSON.stringify(selectedHospital)} onChange={handleHistorySelectedOptionChange}>
 
-                {/* dynamically add an option for each hospital */}
-                {/* NB type-ahead might be nice, might be nice to keep the selection from last time */}
-                {hospitalOptions.map((hospitalOption, index) => (
-                  <option key={index} value={JSON.stringify(hospitalOption)}>
-                    {hospitalOption.name}
-                  </option>
-                ))}
+                  {/* dynamically add an option for each hospital */}
+                  {/* NB type-ahead might be nice, might be nice to keep the selection from last time */}
+                  {hospitalOptions.map((hospitalOption, index) => (
+                    <option key={index} value={JSON.stringify(hospitalOption)}>
+                      {hospitalOption.name}
+                    </option>
+                  ))}
 
-              </select>
-            </td>
+                </select>
+              </td>
 
-            {/* NB could add date range to further filter down the visits, or something like "in the last week"/"in the last month"... */}
+              {/* NB could add date range to further filter down the visits, or something like "in the last week"/"in the last month"... */}
 
-            {/* NB could have hyperlinks to the hospital details, patient details, visit details... */}
-          </tr>
-        </tbody>
-      </table>
-      <br />
-      <button onClick={handleSearchButtonClick} type="submit">Search</button>
-      {/* </form> */}
+              {/* NB could have hyperlinks to the hospital details, patient details, visit details... */}
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <button onClick={handleSearchButtonClick} type="submit">Search</button>
+      </form>
 
       {/* NB could have a "clear" button which resets the form back to how it was at page load time */}
 
